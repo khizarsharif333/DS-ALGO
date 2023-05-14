dcae9011a798d97595f0f82d566cbe4473df3259
S Khizar Sharif <khizarsharif1234@gmail.com>
2023-05-14 13:09:12 +0530

    code 78

--- src/Striver_SDE_Sheet/Code78.java
+++ src/Striver_SDE_Sheet/Code78.java
@@ -3,5 +3,6 @@ package Striver_SDE_Sheet;
 public class Code78 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Code 78");
     }
 }
